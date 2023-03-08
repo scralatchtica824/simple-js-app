2ba8ec36ea5fa2ef2681ebf89ed593453f9bb995
scralatchtica824 <122630961+scralatchtica824@users.noreply.github.com>
2023-03-08 11:40:07 -0500

    Building Exercise 2.0

---
+++ scripts.js
@@ -0,0 +1,24 @@
+// Testing normal comment
+alert('Hello world - Click Okay to see my name!');
+/* Testing a larger comment 
+that takes up 2 or more lines */
+let age = 34;
+let myName = 'Doug';
+document.write(myName);
+
+let simpleAddition = 2+2;
+document.write(simpleAddition)
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+let size2 = 100;
+let doubleSize2 = size2 * 3;
+let minSize = (doubleSize2 * 2) - (size2 / 6);
+document.write(minSize);
+
+let favoriteFood = cheeseBurgers;
+document.write(cheeseBurgers)
+
+//End comments here..
