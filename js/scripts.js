3f0bea533d2568ae12498813939187b837e1e0e6
scralatchtica824 <122630961+scralatchtica824@users.noreply.github.com>
2023-03-24 08:52:18 -0400

    Ran loop to display height and string

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 alert('Hello world - Click here so see the beginning of my Pokémon project!');
 
+//this is defining the name, height, weight and types of pokemon in an array
 let pokemonList = [
     { name: 'Venusaur', height: 2, weight: 100, types: ['grass', 'poison']},
     { name: 'Charizard', height: 1.7, weight: 90.5, types: ['monster', 'dragon']},
     { name: 'Blastoise', height: 1.6, weight: 88.5, types: ['monster', 'water']},
     { name: 'Raichu', height: 0.8, weight: 30, types: ['field', 'fairy']},
   ];
+
+  //loop through to display the name and height of the pokemon on seperate lines
+  for (let i = 0; i < pokemonList.length; i++) {
+    let pokemonData = '<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + 'm)';
+    if (pokemonList[i].height > 1.6){
+      pokemonData += ' - This is a Large Pokemon!'
+    }
+    
+    pokemonData += '</p>';
+    document.write(pokemonData);
+}
