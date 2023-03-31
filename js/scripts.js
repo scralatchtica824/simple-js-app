7d4d7104556c9d1ccfc23bb5a1ff4373a40091ef
scralatchtica824 <122630961+scralatchtica824@users.noreply.github.com>
2023-03-31 09:30:54 -0400

    Update scripts.js
    Updated to complete task 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 alert('Hello world - Click here so see the beginning of my Pokémon project!');
 
-//this is defining the name, height, weight and types of pokemon in an array
-let pokemonList = [
+//this is defining the name, height, weight and types of pokemon in an array, wrapping it in an IIFE and making it print on the screen
+let pokemonRepository = (function () {
+  let pokemonList = [
     { name: 'Venusaur', height: 2, weight: 100, types: ['grass', 'poison']},
     { name: 'Charizard', height: 1.7, weight: 90.5, types: ['monster', 'dragon']},
     { name: 'Blastoise', height: 1.6, weight: 88.5, types: ['monster', 'water']},
     { name: 'Raichu', height: 0.8, weight: 30, types: ['field', 'fairy']},
   ];
 
-pokemonList.forEach(function(pokemon) {
-  document.write('<p>' + pokemon.name + " " + pokemon.height + " " + pokemon.types + '<p>')
-});
+  pokemonList.forEach(function(pokemon) {
+    document.write('<p>' + pokemon.name + " " + pokemon.height + "m " + pokemon.types + '<p>')
+  });
+})();
+
+//getAll function added
+function getAll() {
+  return pokemonList;
+}
+
+//add Function inserted here
+function add(pokemon) {
+  pokemonList.push(pokemon);
+}
 
 
   /*loop through to display the name and height of the pokemon on seperate lines. 
